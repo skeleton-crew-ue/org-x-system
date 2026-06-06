321efbe2ed5a6048901c2f3ec9f49c9b45ca5510
Vladimer Gogia <gogia.vt@gmail.com>
2026-06-06 16:50:55 +0200

    add tag and document models, registered and applied migration

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("title", "uploaded_by", "uploaded_at")
+    list_filter = ("tags", "uploaded_at")
+    search_fields = ("title", "description")
+    filter_horizontal = ("tags",)
+    readonly_fields = ("uploaded_at",)
--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    file = models.FileField(upload_to='documents/%Y/%m/')
+    tags = models.ManyToManyField(Tag, blank=True)
+    uploaded_by = models.ForeignKey(
+        'members.User',
+        on_delete=models.PROTECT,
+        related_name='documents',
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-uploaded_at']
+
+    def __str__(self):
+        return self.title
