7f81e333f4d362d313c3f5038c90f00c2b7b46c5
Harsh Dhiman <harshdhiman5421@gmail.com>
2026-06-19 18:33:35 +0200

    Update urls.py

--- documents/urls.py
+++ documents/urls.py
@@ -6,7 +6,6 @@ app_name = 'documents'
 urlpatterns = [
     path('', views.document_list, name='list'), 
     
-    # Rest of your URLs...
     path('<int:id>/', views.document_detail, name='document_detail'),
     path('<int:id>/download/', views.document_download, name='document_download'),
     path('upload/', views.document_upload, name='document_upload'),
