24f2dea009355171374b1c6c95b36584411c6af2
gogiavt-collab <gogia.vt@gmail.com>
2026-06-19 07:14:29 +0200

    add voting module: ballot, option, vote (#13)

--- voting/admin.py
+++ voting/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ballot, BallotOption, Vote
+
+
+class BallotOptionInline(admin.TabularInline):
+    model = BallotOption
+    extra = 2
+
+
+@admin.register(Ballot)
+class BallotAdmin(admin.ModelAdmin):
+    list_display = ("title", "opens_at", "closes_at", "is_active")
+    list_filter = ("is_active",)
+    inlines = [BallotOptionInline]
+    readonly_fields = ("created_by",)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Vote)
