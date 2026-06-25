bc2d4d2044810e00ebfb47d98d1d7cff10d39a5a
Vladimer Gogia <gogia.vt@gmail.com>
2026-06-25 09:48:57 +0200

    add full text search for documents

--- documents/signals.py
+++ documents/signals.py
@@ -1,13 +1,17 @@
+from django.contrib.postgres.search import SearchVector
+from django.db import connection
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.postgres.search import SearchVector
 
 from .models import Document
 
+
 @receiver(post_save, sender=Document)
 def update_search_vector(sender, instance, **kwargs):
+    if connection.vendor != 'postgresql':
+        return
+
     Document.objects.filter(id=instance.id).update(
         search_vector=SearchVector('title', weight='A') +
                       SearchVector('description', weight='B')
     )
-    
