24556dba8b1026018383cf6162f4a041a36c140d
Danna <dannakiara7944@gmail.com>
2026-07-02 00:48:14 +0200

    Configuración inicial de base de datos y ajustes de entorno

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ CSRF_TRUSTED_ORIGINS = env.list(
 
 # --- Applications --------------------------------------------------------
 INSTALLED_APPS = [
+    "django.contrib.postgres",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
