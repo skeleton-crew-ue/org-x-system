0f47c28355c5a0fc9febc818e440f0b468580bf6
gogiavt-collab <gogia.vt@gmail.com>
2026-07-09 19:21:15 +0200

    Cleanup: remove dead Broadcast admin entry, link finance summary (#34)
    * Remove dead whatsapp.Broadcast admin registration
    
    * Add Summary link to finance list page

--- whatsapp/admin.py
+++ whatsapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Broadcast, ChatAnalysis, ChatExport
+from .models import ChatAnalysis, ChatExport
 
 
 @admin.register(ChatExport)
@@ -14,10 +14,3 @@ class ChatExportAdmin(admin.ModelAdmin):
 class ChatAnalysisAdmin(admin.ModelAdmin):
     list_display = ("chat_export", "generated_at")
     list_filter = ("generated_at",)
-
-
-@admin.register(Broadcast)
-class BroadcastAdmin(admin.ModelAdmin):
-    list_display = ("subject", "sent_by", "sent_at", "recipient_filter", "recipient_count")
-    search_fields = ("subject",)
-    list_filter = ("recipient_filter", "sent_at")
